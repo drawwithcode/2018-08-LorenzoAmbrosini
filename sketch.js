5e555bebae44d5d4c3da3ec0dc5d5c4a6c8d1baa
Lorenzo Ambrosini <lorenzo.ambrosini@mail.polimi.it>
2018-11-17 16:56:59 +0100

    lavatrice

--- sketch.js
+++ sketch.js
@@ -1,11 +1,30 @@
+var speedY = windowHeight/2;
+var speedX = windowWidth/2;
 function preload(){
   // put preload code here
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-  // put drawing code here
+
+
+  background(100);
+
+  ballMove();
+
+}
+
+function ballMove(){
+
+  var speedY = map(rotationX, -60, 90, 0, windowHeight);
+  var speedX = map(rotationY, -45, 45, 0, windowWidth);
+  
+  fill('red');
+  ellipse(speedX, speedY, 50);
+
+
+
 }
